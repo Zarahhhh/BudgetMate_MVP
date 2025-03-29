7a425f246cacd7942fcdb442cb1a1f96de085ff1
Zarahhhh <zakariyyahrahmat@gmail.com>
2025-03-29 21:07:28 +0100

    Updated expense formatting and improvements

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/expenses";
+const API_URL = "https://budgetmate-mvp.onrender.com/api/expenses";
 
 export const fetchExpenses = async () => {
     const res = await axios.get(API_URL);
